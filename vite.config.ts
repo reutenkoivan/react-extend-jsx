91898758abeb6063e74dc3aef28e7e0660c73d6a
Ivan Reutenko <reutenkoivan@gmail.com>
2024-03-30 17:32:13 +0200

    buyild

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		dts({
+			include: ['src'],
+		}),
+	],
 	build: {
 		lib: {
+			fileName: 'main',
 			entry: path.resolve(__dirname, 'src', 'main.ts'),
 			formats: ['es'],
+		},
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
 		},
 	},
 })
